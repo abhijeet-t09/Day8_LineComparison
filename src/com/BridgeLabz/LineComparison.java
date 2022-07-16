de64219d3b13f54e1f6309a58bc4d2c2b074a5c6
abhijeet-t09 <tripathi.abhijeet0996@gmail.com>
2022-07-16 09:05:11 +0530

    Welcome to Line Comparison Program

---
+++ src/com/BridgeLabz/LineComparison.java
@@ -0,0 +1,8 @@
+package com.BridgeLabz;
+import java.util.*;
+
+public class LineComparison {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Line Comparison Computation Program");
+    }
+}
